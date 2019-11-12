ab273ee97104289d0a6412e73e94683eeb1d8c80
EdwinRoquet <eroquetf@gmail.com>
2019-11-12 00:19:13 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 // if( process.env.NODE_ENV ==='dev' ){
 //     urlDB = 'mongodb://localhost:27017/cafe';
 // }else{
-    urlDB ='mongodb+srv://user07:PkusiFjkYHNGrEFm@cluster0-wyxfx.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 // }
 
 
